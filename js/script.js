7e9a936ae5a099f2b8180fc371efe58567824eae
khalilnasri92@gmail.com <Kh@lil92>
2021-11-16 13:30:07 +0100

    fix linters errors

--- js/script.js
+++ js/script.js
@@ -2,20 +2,19 @@ const btnOpen = document.querySelector('.hamburger-btn');
 const btnClose = document.querySelector('.close-btn');
 const menu = document.querySelector('.nav-items');
 const menuItem = document.querySelectorAll('.menu-item');
-const instructorsSection = document.querySelector('#instructors-list');
 
 function openMenu() {
-    btnOpen.style.display = 'none';
-    menu.style.display = 'block';
+  btnOpen.style.display = 'none';
+  menu.style.display = 'block';
 }
 
 function closeMenu() {
-    btnOpen.style.display = 'block';
-    menu.style.display = 'none';
+  btnOpen.style.display = 'block';
+  menu.style.display = 'none';
 }
 
 btnOpen.addEventListener('click', openMenu);
 btnClose.addEventListener('click', closeMenu);
 menuItem.forEach((item) => {
-    item.addEventListener('click', closeMenu);
-})
+  item.addEventListener('click', closeMenu);
+});
